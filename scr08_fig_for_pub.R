d7db4fbda4537c7eeb987e239642847908c2d574
Chuck Burks <r.dominica@gmail.com>
2025-07-08 10:35:20 -0700

    Survival plots combined and saved as jpg

--- scr08_fig_for_pub.R
+++ scr08_fig_for_pub.R
@@ -1,4 +1,18 @@
 # scr08_fig_for_pub.R
 
-# Add two figures together to make a single figure for the journal
+library(cowplot)
+library(ggplot2)
 
+# Open saved plot objects
+load("surv_plots.RData")
+
+# Plots--p1 is 0 and 300 Gy, p2 is males vs females
+p_dose <- p1$plot
+p_sex <- p2$plot
+
+# use cowplot::plot_grid to get the plots side-by-side
+combined_plot <- plot_grid(p_dose,p_sex, ncol = 2, labels = "auto")
+
+# use ggsave to save as the desired jpg
+ggsave("combined_plot.jpg", combined_plot, width = 8.3, height = 4.98,
+       dpi = 300)
