3ed80a9f801e94e390dbfce1e9d57385bbac19ed
Chuck Burks <r.dominica@gmail.com>
2025-09-08 14:31:52 -0700

    Generate plot as BW tiff for ms; tiff 2LR 4 GH

--- scr08_fig_for_pub_scratch.R
+++ scr08_fig_for_pub_scratch.R
@@ -2,6 +2,7 @@
 
 library(cowplot)
 library(ggplot2)
+library(grid)     # for unit()
 
 # Open saved plot objects
 load("surv_plots.RData")
@@ -10,20 +11,13 @@ load("surv_plots.RData")
 p_dose <- p1$plot
 p_sex <- p2$plot
 
-# Change to black-and-white color scheme
-  p_dose <- p1$plot + 
-    scale_color_manual(values = c("black", "black")) +
-    scale_linetype_manual(values = c("solid", "dashed")) +
-    theme_bw()
-
-# p_sex <- p2$plot +
-#   scale_color_manual(values = c("black", "black")) +
-#   scale_linetype_manual(values = c("solid", "dashed")) +
-#   theme_bw()
 
 # use cowplot::plot_grid to get the plots side-by-side
 combined_plot_bw <- plot_grid(p_dose,p_sex, ncol = 2, labels = "auto")
 
 # use ggsave to save as the desired jpg
-ggsave("combined_plot_bw.jpg", combined_plot_bw, width = 8.3, height = 4.98,
+ggsave("combined_plot_bw.tif", 
+       plot = combined_plot_bw, 
+       device = "tiff",
+       width = 8.3, height = 4.98,
        dpi = 300)
